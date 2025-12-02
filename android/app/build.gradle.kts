1b284666bbbda41157ec3f1deb8ce58abeb9b143
Huzaifa7293 <huzaifaahmedmehar@gmail.com>
2025-12-03 00:45:40 +0500

    Version Code Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         targetSdk = 36
 //        versionCode = flutter.versionCode
 //        versionName = flutter.versionName
-        versionCode = 20
+        versionCode = 21
         versionName = "1.1.4"
     }
 
